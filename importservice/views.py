ef83bbd5938c0aaea5e7a9a95838a86fb91a8f4a
mauriciodev <mauricio.dev@gmail.com>
2025-05-19 16:00:15 -0300

    Primeira view

--- importservice/views.py
+++ importservice/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    # Obtém o valor do parâmetro "nome" enviado na query string
+    name = request.GET.get('nome', 'IME')  # valor padrão: "Visitante"
+    html = '<html lang="en"><body>Boa tarde,  %s.</body></html>' % name
+    return HttpResponse(html)
 
 # Create your views here.
