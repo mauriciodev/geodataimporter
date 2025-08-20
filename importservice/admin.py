960f8faa01f02d1bd68c40eb2b7d3053475d95c2
gns2025 <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>
2025-08-20 16:17:00 -0300

    Reorganizando o admin.py e adicionando configuração para todos os modelos
    1) Adicionados ao Django Admin os modelos HistoricoImportacaoExclusao, ProdutoGeoespacial e ProductIndex.
    
    2) Incluída personalização de exibição (list_display, list_filter, search_fields, ordering) para cada modelo.
    
    3) Atualizada a configuração de RepresentacaoGrafica, substituindo o registro simples por uma versão personalizada.

--- importservice/admin.py
+++ importservice/admin.py
@@ -1,6 +1,47 @@
 from django.contrib import admin
+from importservice.models import (
+    RepresentacaoGrafica,
+    HistoricoImportacaoExclusao,
+    ProdutoGeoespacial,
+    ProductIndex
+)
 
-from importservice.models import RepresentacaoGrafica
+# -----------------------------
+# Representacao Grafica Admin
+# -----------------------------
+@admin.register(RepresentacaoGrafica)
+class RepresentacaoGraficaAdmin(admin.ModelAdmin):
+    list_display = ['esquema', 'classe', 'grupo_representacao']
+    list_filter = ['esquema', 'grupo_representacao']
+    search_fields = ['classe', 'esquema']
+    ordering = ['esquema', 'classe']
 
-# Register your models here.
-admin.site.register(RepresentacaoGrafica)
+# -----------------------------
+# Historico Importacao/Exclusao Admin
+# -----------------------------
+@admin.register(HistoricoImportacaoExclusao)
+class HistoricoImportacaoExclusaoAdmin(admin.ModelAdmin):
+    list_display = ['metadata_id', 'classe', 'acao', 'usuario', 'data_evento']
+    list_filter = ['acao', 'usuario', 'data_evento']
+    search_fields = ['metadata_id', 'classe', 'detalhes']
+    ordering = ['-data_evento']
+
+# -----------------------------
+# Produto Geoespacial Admin
+# -----------------------------
+@admin.register(ProdutoGeoespacial)
+class ProdutoGeoespacialAdmin(admin.ModelAdmin):
+    list_display = ['metadata_id', 'nome_arquivo', 'data_do_produto', 'data_importacao', 'esquema', 'escala']
+    list_filter = ['esquema', 'escala', 'data_importacao']
+    search_fields = ['metadata_id', 'nome_arquivo', 'esquema']
+    ordering = ['-data_importacao']
+
+# -----------------------------
+# Product Index Admin
+# -----------------------------
+@admin.register(ProductIndex)
+class ProductIndexAdmin(admin.ModelAdmin):
+    list_display = ['metadataid', 'file_path', 'date', 'scale']
+    list_filter = ['date', 'scale']
+    search_fields = ['metadataid', 'file_path']
+    ordering = ['-date']
